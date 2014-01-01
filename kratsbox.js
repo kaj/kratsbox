f1bfbe055e1a54a8eff04b1c4280edbf687e85a4
Rasmus Kaj <kaj@kth.se>
2014-01-01 23:25:59 +0100

    Basically work (in firefox) without jQuery.

--- kratsbox.js
+++ kratsbox.js
@@ -1,10 +1,26 @@
-(function($) {
-    var settings, links, root = false, img,
-    methods = {
+if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+
+    function kratsbox(selector, options) {
+        console.debug("In initkratsbox for selector '" + selector + "',",
+                      options);
+
+        function create(htmlStr) {
+            var frag = document.createDocumentFragment(),
+            temp = document.createElement('div');
+            temp.innerHTML = htmlStr;
+            while (temp.firstChild) {
+                frag.appendChild(temp.firstChild);
+            }
+            return frag;
+        }
+
+        var settings, data = {}, root = false,
+        methods = {
         open : function() {
+            root = document.querySelector('#kratsbox');
+            console.debug('Open called, root:', root, ' data:', data);
             if(!root) {
-                root =
-                    $('<div id="kratsbox"><div class="bg"></div>'+
+                root = create('<div id="kratsbox"><div class="bg"></div>'+
                       '<div id="krbxframe"><img alt="">'+
                       '<p><a href="#close" class="close" title="close">'+
                       settings['close']+'</a>'+
@@ -13,131 +29,153 @@
                       settings['prev']+'<span class="extra"></span></a>'+
                       '<a href="#next" class="next" title="next image">'+
                       settings['next']+'<span class="extra"></span></a>'+
-                      '</span><span id="krbxcaption"></span></p></div></div>')
-                    .appendTo('body');
-                img = root.find('img');
-                var close = root.find('.close'),
-                next = root.find('.next'),
-                prev = root.find('.prev');
-                close.bind('click.kratsbox', methods.close);
-                next.bind('click.kratsbox', methods.next);
-                prev.bind('click.kratsbox', methods.prev);
-                function setupfocus(elem, nextelem, prevelem) {
-                    elem.keydown(function(event){
-                        if(event.which == 9) {
-                            if(event.shiftKey) {
-                                prevelem.focus();
-                            } else {
-                                nextelem.focus();
-                            } 
-                            event.stopPropagation();
-                            return false;
-                        }
-                    });
-                }
-                if (links.length > 1) {
-                    setupfocus(close, prev, next);
-                    setupfocus(next, close, prev);
-                    setupfocus(prev, next, close);
-                } else {
-                    prev.hide();
-                    next.hide();
-                    setupfocus(close, close, close);
-                }
-                root.keydown(function(event){
-                    switch(event.which) {
-                    case 9: // tab
-                        close.focus();
-                        break;
-                    case 27: // escape
-                        methods.close();
-                        break;
-                    case 37: // left arrow
-                    case 38: // up arrow
-                    case 33: // pgup
-                    case 78: // 'n'
-                        methods.prev();
-                        break;
-                    case 39: // right arrow
-                    case 40: // down arrow
-                    case 34: // pgdn
-                    case 80: // 'p'
-                        methods.next();
-                        break;
-                    default:
-                        return true;
-                    }
-                    event.stopPropagation();
-                    return false;
-                });
+                      '</span><span id="krbxcaption"></span></p></div></div>');
+
+                document.body.appendChild(root);
+                root = document.querySelector('#kratsbox');
+                var img = root.querySelector('img');
                 function limitSize() {
-                    if (img.width()) {
-                        root.find('#krbxframe').width('auto');
-                        img.css('max-height', (root.height()-120)+'px');
-                        root.find('#krbxframe').width(img.width());
-                        root.find('.extra').height(img.height());
+                    if (img.clientWidth) {
+                        var kf = root.querySelector('#krbxframe');
+                        kf.style.width = 'auto';
+                        img.style.maxHeight = (root.clientHeight-120)+'px';
+                        kf.style.width = img.clientWidth + 'px';
+                        kf.querySelector('.extra').style.height = img.clientHeight + 'px';
                     } else {
                         window.setTimeout(limitSize, 100);
                     }
                 }
-                $(window).resize(limitSize);
-                img.bind('load', limitSize);
+                console.debug("Img:", img)
+                window.addEventListener('resize', limitSize);
+                img.addEventListener('load', limitSize);
                 limitSize();
             }
+
+            var close = root.querySelector('.close'),
+            next = root.querySelector('.next'),
+            prev = root.querySelector('.prev');
+            close.onclick = methods.close;
+            next.onclick = methods.next;
+            prev.onclick = methods.prev;
+            function setupfocus(elem, nextelem, prevelem) {
+                elem.onkeydown = function(event) {
+                    if(event.which == 9) {
+                        if(event.shiftKey) {
+                            prevelem.focus();
+                        } else {
+                            nextelem.focus();
+                        } 
+                        event.stopPropagation();
+                        return false;
+                    }
+                };
+            }
+            if (data.links.length > 1) {
+                prev.style.display = 'inline';
+                next.style.display = 'inline';
+                setupfocus(close, prev, next);
+                setupfocus(next, close, prev);
+                setupfocus(prev, next, close);
+            } else {
+                prev.style.display = 'none';
+                next.style.display = 'none';
+                setupfocus(close, close, close);
+            }
+            root.onkeydown = function(event) {
+                switch(event.which) {
+                case 9: // tab
+                    close.focus();
+                    break;
+                case 27: // escape
+                    methods.close();
+                    break;
+                case 37: // left arrow
+                case 38: // up arrow
+                case 33: // pgup
+                case 78: // 'n'
+                    methods.prev();
+                    break;
+                case 39: // right arrow
+                case 40: // down arrow
+                case 34: // pgdn
+                case 80: // 'p'
+                    methods.next();
+                    break;
+                default:
+                    return true;
+                }
+                event.stopPropagation();
+                return false;
+            };
             methods.loaddata(this);
-            root.show();
-            root.find('.bg').fadeTo(200, 0.8);
-            root.find('#krbxframe').show();
-            root.find('.close').focus();
+            //root.find('.bg').fadeTo(200, 0.8);
+            root.style.display = 'block';
+            root.querySelector('#krbxframe').style.display = 'block';
+            document.querySelector('#krbxframe .close').focus();
+            console.debug("open method returning false");
             return false;
         },
         next: function() {
-            var i = img.data('current') + 1
-            methods.loaddata(links[i % links.length]);
+            var i = data.current + 1
+            methods.loaddata(data.links[i % data.links.length]);
             return false;
         },
         prev: function() {
-            var i = img.data('current') - 1
-            methods.loaddata(links[(i+links.length) % links.length]);
+            var i = data.current - 1
+            methods.loaddata(data.links[(i+data.links.length) % data.links.length]);
             return false;
         },
         loaddata: function(selected) {
-            var s = $(selected), cap = s.attr('title'),
-                capE = root.find('#krbxcaption');
-            img.data('current', s.data('krbxindex'));
-            img.attr('src', s.attr('href'));
+            console.debug('Loaddata for', selected, 'root:', root);
+            var img = root.querySelector('img');
+            var cap = selected.getAttribute('title'),
+                capE = document.querySelector('#krbxcaption');
+            data.current = selected.getAttribute('data-krbxindex');
+            img.setAttribute('src', selected.getAttribute('href'));
             if (cap) {
-                capE.text(cap);
+                capE.innerHTML = cap;
             } else {
-                capE.html(s.parent('figure').find('figcaption').html() || '');
+                var capSrc = selected.parentNode.querySelector('figcaption');
+                if (capSrc) {
+                    capE.innerHTML = capSrc.innerHTML;
+                }
             }
+            console.debug('Loaddata done');
         },
         close : function() {
-            root.find('.bg').fadeOut(400, function(){root.hide()});
-            root.find('#krbxframe').hide();
-            $(links[img.data('current')]).focus();
+            //root.find('.bg').fadeOut(400, function(){root.hide()});
+            document.querySelector('#krbxframe').style.display = 'none';
+            document.querySelector('#kratsbox').style.display = 'none';
+            data.links[data.current].focus();
             return false;
         }
-    };
-    $.fn.kratsbox = function(options) {
-        settings = $.extend({
+        };
+
+        settings = {
             'minsize': 500,
             'next': 'next \u2192',
             'prev': '\u2190 prev',
             'close': 'close \u00D7'
-        }, options);
-        
-        if ($(window).width() < settings.minsize ||
-            $(window).height() < settings.minsize) {
+        };
+        for (var attrname in options) {
+            settings[attrname] = options[attrname];
+        }
+
+        if (window.innerWidth < settings.minsize ||
+            window.innerHeight < settings.minsize) {
             
             return this;
         }
         
-        links = this;
-        links.each(function(index) {
-            $(this).data('krbxindex', index);
+        data.links = document.querySelectorAll(selector);
+        console.log("Selector:", selector, "Links: ", data.links);
+        var i = 0;
+        [].forEach.call(data.links, function(link) {
+            console.debug("Initializing", link);
+            link.setAttribute('data-krbxindex', i++);
+            link.onclick = methods.open;
         });
-        links.bind('click.kratsbox', methods.open);
+        console.log('Data:', data);
         return this;
     };
-})(jQuery);
+}
